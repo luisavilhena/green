55d889f37002ec99f1566f98c3f119f67232918b
Habemus <hello@habem.us>
2020-03-22 14:52:04 -0300

    adiciona scroll

--- public/js/index.js
+++ public/js/index.js
@@ -5,11 +5,92 @@ const $sectionHome = jQuery('#green-section-home')
 const greenCarouselMobile = document.querySelector('.green-carousel-mobile')
 const greenCarouselDesktop = document.querySelector('.green-carousel-desktop')
 
+// Crio uma variavel para salvar a resposta da primeira pergunta
+let haveSunlight = undefined;
+let haveWater = undefined;
+let havePets = undefined;
+// pego todas as respostas da primeira pergunta, e adiciono um evento de clique nelas
+const $sunlightAnswerHight = jQuery(".sunlight_answer_hight");
+const $sunlightAnswerLow = jQuery(".sunlight_answer_low");
+const $sunlightAnswerNo = jQuery(".sunlight_answer_no");
+const $waterAnswerRarely = jQuery(".water_answer_rarely");
+const $waterAnswerRegularly = jQuery(".water_answer_regularly");
+const $waterAnswerDaily = jQuery(".water_answer_daily");
+const $petsAnswerFalse = jQuery(".pets_answer_false");
+const $petsAnswerTrue = jQuery(".pets_answer_true");
+const $nextFilterButton = jQuery(".next_filter_button");
+const $buyNow = jQuery(".buy_now_button");
+
+$sunlightAnswerHight.click((e) => {
+	haveSunlight = 'high'
+});
+
+$sunlightAnswerLow.click(e => {
+  haveSunlight = "low";
+});
+
+$sunlightAnswerNo.click(e => {
+	haveSunlight = "no";
+});
+
+$waterAnswerRarely.click(e => {
+  haveWater = "rarely";
+})
+
+$waterAnswerRegularly.click(e => {
+  haveWater = "regularly";
+})
+
+$waterAnswerDaily.click(e => {
+  haveWater = "Daily";
+})
+
+$petsAnswerFalse.click(e => {
+  havePets = false;
+})
+
+$petsAnswerTrue.click(e => {
+  havePets = true;
+})
+
+$nextFilterButton.click(e => {
+  if (haveSunlight === undefined){
+    return
+  } else if (haveWater === undefined) {
+    return
+  } else if (havePets === undefined) {
+    return
+  } else {
+    $nextFilterButton.attr('href', '#green-section-picks')
+  }
+})
+
+$buyNow.click(e => {
+  $buyNow.attr('href', 'green-section-contact')
+})
+
+
+// Quando clicarmos na ultima seta do carousel, ai sim devemos mostrar os resultas e chamar uma funcao para pegar o JSON do backend:
+
+// Olhe para o email de novo, e preste atenção como o backend espera a resposta:
+//Parâmetros e valores aceitos
+
+//sun :high , low ou no
+//water: daily , regularly ou rarely
+//pets: false ou true
+
+// EXEPLO DE CHAMADA (olha bem para o final da chamada, como tem as variaveis que vamos enviar): https://6nrr6n9l50.execute-api.us-east-1.amazonaws.com/default/front-plantTest-service?sun=high&water=rarely&pets=false
+const pegarResultados = () => {
+	const escolhasUsuario =
+    "sun=" + haveSunlight + "&water=" + havePets + "&pets" + havePets;
+}
+
+
 $('.carousel').carousel()
 
 $('.carousel').on('slid', '', function() {
   var $this = $(this);
-
+	console.log('haveSunlight');
   $this.children('.carousel-control').show();
 
   if($('.carousel-item:first').hasClass('active')) {
@@ -20,8 +101,6 @@ $('.carousel').on('slid', '', function() {
   }
 
 });
-
-console.log(greenLogoRotate)
 
 const controlResponsive =  () => {
 	if ($window.width() < 708) {
